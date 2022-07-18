97b942015ef952ccf36e782075d61bdb3b31f65c
shirotamaki <shiro.tamaki63@gmail.com>
2022-07-18 21:42:24 +0900

    Fix: プレースホルダを追加しSQLインジェクション対策を実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,7 +10,8 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where("title LIKE '%#{term}%' OR content LIKE '%#{term}%'")
+    where("title = ?", term)
+      .or(where("content = ?", term))
   }
 
   scope :default_order, -> { order(id: :desc) }
