0f324c7c153b59cdfcd8710f51fbce3db4f9e834
shirotamaki <shiro.tamaki63@gmail.com>
2022-07-26 13:07:19 +0900

    Fix: プレミアムユーザー以外は有料機能を使えないように設定を変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -54,7 +54,11 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    if current_user.premium?
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 
   def empower_user_change
